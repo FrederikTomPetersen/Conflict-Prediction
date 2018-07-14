dad0bb94d36699cc15405f44ecd47cc589541657
FrederikTomPetersen <FrederikTomPetersen@users.noreply.github.com>
2018-07-14 12:49:02 +0200

    testing
    Playing with the GDELTR2 full text API

--- FullTextAPI.R
+++ FullTextAPI.R
@@ -55,3 +55,53 @@ View(df_countries)
 records <- get_data_ft_v2_api(terms = my_terms, domains = my_domains, 
                    gkg_themes = my_themes, 
                    modes = c("Artlist"), timespans = my_timespan)
+
+
+
+
+terms <- c('"war"', '"protest"', '"instability"')
+web_sites <- c("nypost.com", "washingtonpost.com", "wsj.com")
+get_data_ft_v2_api(terms = terms, domains = web_sites, timespans = "28 Weeks")
+trelliscopeImage
+
+parse_gkg_mentioned_people(trelliscopeImage)
+
+
+
+
+
+
+location_codes <-
+  get_codes_stability_locations()
+location_test <-
+  get_data_locations_instability_api(
+    location_ids = c("US", "IS", "CA", "TU", "CH", "UK", "IR"),
+    use_multi_locations = c(T, F),
+    variable_names = c('tone', 'protest', 'conflict'),
+    time_periods = c('daily'),
+    nest_data = F,
+    days_moving_average = NA,
+    return_wide = T,
+    return_message = T
+  )
+
+location_test %>%
+  dplyr::filter(codeLocation %>% is.na()) %>%
+  group_by(nameLocation) %>%
+  summarise_at(.vars = c('instability', 'tone', 'protest', 'conflict'),
+               funs(mean)) %>%
+  arrange(desc(instability))
+
+
+
+abc <- get_data_locations_instability_api(location_ids = c('US', 'IS', "TU"), 
+                                   random_locations = NULL, 
+                                   variable_names = c('instability', 'conflict', 'tone', 'protest', 'artvolnorm'), 
+                                   days_moving_average = NA, 
+                                   time_periods = 'daily', 
+                                   use_multi_locations = F, 
+                                   return_wide = T, 
+                                   nest_data = F, 
+                                   return_message = T)
+
+range(abc$value, na.rm = FALSE)
