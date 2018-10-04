fb8617a579fb1de8182f5536e96b2b28b896cd40
FrederikTomPetersen <36158205+FrederikTomPetersen@users.noreply.github.com>
2018-10-04 19:14:32 +0200

    Extracting of relevant data from Fearon Laitin

--- Fearon Laitin.R
+++ Fearon Laitin.R
@@ -1,5 +1,9 @@
 # Hentning af Fearon & Laitens data fra 2003
 
+library(tidyverse)
+library(data.table)
+
+
 setwd(DataCave)
 direct_link <-  "https://web.stanford.edu/group/ethnic/publicdata/repdata.zip"
 download.file(direct_link, basename(direct_link))
@@ -15,6 +19,23 @@ fldata <- data %>%
 rm(data)
 
 
-a <- fldata %>% 
-  distinct(ccode)  # vi har 161 lande
-rm(a)
+
+
+mountains <- fldata %>% 
+  distinct(ccode, mtnest)  # vi har 161 obs
+
+mountains <- fldata %>% 
+  distinct(ccode, mtnest)
+
+ethnicfractionalization <- fldata %>% 
+  distinct(ccode, ethfrac) #345 og 365 optræder to gange (Rusland og Jugoslavien) skal jeg abre tage middelværdien? 
+
+religiousfractionalization <-  fldata %>% 
+  distinct(ccode,relfrac)
+
+oil <- fldata %>% 
+  distinct(ccode, Oil) # her har vi 184 variable, hvilket udtrykker at et land ikek altid har væres olieeksporterende
+
+colwars <-  fldata %>% 
+  distinct(ccode, colwars) #her får jeg 301 observatoner, hvilket udtrykker at der har været koloniale krige og ikek krige gennem tidsperioden 
+
