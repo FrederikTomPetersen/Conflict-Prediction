3ae63eb3c8f0696a3120de1629a07b004b320ec6
FrederikTomPetersen <FrederikTomPetersen@users.noreply.github.com>
2018-11-07 22:27:23 +0100

    Visualization

--- 7 Visulization.R
+++ 7 Visulization.R
@@ -4,6 +4,155 @@ cat("\014")
 ##                                       Visualizing                                         ##
 ################################################################################################
 
+# Udviklingen i antallet af døde relaterede til instrastatslige kamphandlinger
+
+ged <-  dbGetQuery(con, "SELECT * from ged_aggregated")
+
+plotdata <- ged %>% 
+  group_by(year) %>% 
+  summarise(deathyear = sum(deaths))
+
+
+plotdata %>% 
+  ggplot(aes(x = year, y = deathyear/1000)) +
+  geom_line()+
+  geom_point() + 
+  labs( y = "Kamprelaterede døde - i tusinder", x="Årstal") +
+  theme_classic() +
+  theme(legend.position="right",panel.background = element_blank())+
+  theme(plot.title = element_text(hjust = 0.5), panel.grid.major.y = element_line(color = "grey80"))+
+  scale_color_gradient()+
+  scale_shape_manual(15) +
+  scale_x_continuous(breaks=c(1989,1995,2000,2005,2010,2015,2017)) +
+  scale_y_continuous(breaks= c(25,50,100,200,300,400,500,600))
+setwd(Latexfigure)
+ggsave(filename = "udviklingidde.pdf" )
+  
+rm(ged,plotdata)
+
+
+
+####################################################################
+# Descriptive data between conflict countries and peace countries  #
+####################################################################
+Country <-  codelist_panel %>% 
+  select(country.name.en,iso2c) %>% 
+  distinct(country.name.en,iso2c) %>% 
+  filter(!is.na(iso2c))
+iso2clist <- Country$iso2c
+
+CountryCodelistPanel <-  codelist_panel %>% 
+  select(p4n, fips) %>% 
+  filter(!is.na(p4n), !is.na(fips)) %>% 
+  distinct(p4n, fips)
+
+GedAggregated <-  dbGetQuery(con, "SELECT * from ged_aggregated")
+
+country_conflict <- GedAggregated %>% 
+  filter(cwy==1) %>% 
+  distinct(p4n) %>% 
+  left_join(CountryCodelistPanel, by=c("p4n"="p4n"))
+
+country_peace <-  GedAggregated %>% 
+  distinct(p4n) %>% 
+  anti_join(country_conflict, by=c("p4n"="p4n"))
+
+
+
+WDI_growth = WDI(indicator='NY.GDP.MKTP.KD.ZG', start=1989, end=2017,  country = 'all') %>% 
+  filter(iso2c %in% iso2clist)
+
+WDI_GDP = WDI(indicator='NY.GDP.PCAP.CD', start=1989, end=2017,  country = 'all') %>% 
+  filter(iso2c %in% iso2clist)
+
+
+GinC <- WDI_growth %>% 
+  filter(iso2c %in% country_conflict$fips & !is.na(NY.GDP.MKTP.KD.ZG)) 
+GinC <- mean(GinC$NY.GDP.MKTP.KD.ZG) %>% 
+  round(digits = 2)
+
+GinP <- WDI_growth %>% 
+  filter(!iso2c %in% country_conflict$fips & !is.na(NY.GDP.MKTP.KD.ZG)) 
+GinP <- mean(GinP$NY.GDP.MKTP.KD.ZG) %>% 
+  round(digits = 2)
+
+GDPinC <-  WDI_GDP %>% 
+  filter(iso2c %in% country_conflict$fips & !is.na(NY.GDP.PCAP.CD)) 
+GDPinC <- mean(GDPinC$NY.GDP.PCAP.CD) %>% 
+  round(digits = 0)
+
+GDPinP <-  WDI_GDP %>% 
+  filter(!iso2c %in% country_conflict$fips & !is.na(NY.GDP.PCAP.CD)) 
+GDPinP <- mean(GDPinP$NY.GDP.PCAP.CD) %>% 
+  round(digits = 0)
+
+
+
+table <-  matrix(c(GDPinC,GinC,GDPinP,GinP), ncol=2)
+colnames(table) <- c('BNP','Vækst' )
+rownames(table) <- c('Lande med konflikt i perioden', 'Lande med fred i perioden')
+setwd(Latextable)
+print(xtable(table, type = "latex"), file = "tabel1.tex")
+
+rm(GinP,GinC,GDPinC,GDPinP,table,country_conflict,country_peace, GedAggregated,WDI_GDP,WDI_growth,Countries,CountryCodelistPanel,Country)
+
+
+
+
+
+
+
+
+#####################################################################
+#                           TIMELINE
+#####################################################################
+
+GedAggregated <-  dbGetQuery(con, "SELECT * from ged_aggregated")
+
+
+timeline <-GedAggregated %>%
+  filter(cwy == 1) %>%
+  group_by(p4n, year) %>%
+  arrange(p4n, year, month) %>% 
+  filter(row_number() == 1) %>%
+  group_by(p4n) %>%
+  mutate(is_contiguous = (year -1==lag(year))) 
+
+timeline <- timeline %>%
+  mutate(is_contiguous = case_when(is.na(is_contiguous) ~ FALSE,
+                                   TRUE~is_contiguous)) %>%
+  mutate(gvar = cumsum(!is_contiguous)) %>%
+  group_by(p4n, gvar) %>%
+  mutate(minyear = min(year),
+         maxyear = max(year)) %>%
+  ungroup() %>%
+  filter(is_contiguous == F | is.na(is_contiguous))
+
+
+
+timeline$country.name.en = as.factor(timeline$country.name.en)
+timeline$country.name.en = fct_reorder(timeline$country.name.en, -timeline$minyear)
+
+
+ggplot(timeline) +
+  geom_segment(aes(x=minyear, xend=maxyear, y=country.name.en, yend=country.name.en), size=0.8) +
+  geom_point(mapping=aes(x=minyear, y=country.name.en), size=2, shape=21, fill="white") +
+  geom_point(mapping=aes(x=maxyear, y=country.name.en), size=2, shape=21, fill="white")+
+  theme_classic() +
+  theme(legend.position="right",panel.background = element_blank())+
+  theme(plot.title = element_text(hjust = 0.5), panel.grid.major.y = element_line(color = "grey80"))+
+  theme(text = element_text(size=8, lineheight=10)) +
+  labs(y = "", x="") +
+  scale_color_gradient() +
+  theme(axis.text=element_text(size=8),
+          axis.title=element_text(size=10,face="bold")) +
+  scale_x_continuous(breaks=c(1989,1995,2000,2005,2010,2015,2017)) 
+
+setwd(Latexfigure)
+ggsave(filename = "timeline.pdf" )
+rm(GedAggregated, timeline)
+
+
 
 
 
